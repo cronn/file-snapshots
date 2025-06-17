c6862458d58e638edcc8aa90a4ef76828f099490
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2025-06-17 10:00:08 +0200

    chore(vitest): Migrate module augmentation to Matchers interface

--- packages/vitest-file-snapshots/src/register.ts
+++ packages/vitest-file-snapshots/src/register.ts
@@ -2,8 +2,7 @@ import type { VitestValidationFileMatchers } from "./matchers/types";
 
 declare module "vitest" {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  interface Assertion<T = any> extends VitestValidationFileMatchers<T> {}
-  interface AsymmetricMatchersContaining extends VitestValidationFileMatchers {}
+  interface Matchers<T = any> extends VitestValidationFileMatchers<T> {}
 }
 
 export { registerValidationFileMatchers } from "./matchers/register-matchers";
