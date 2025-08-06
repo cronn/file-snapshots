c7efd135fe31b167f7f98ba024a55c57ec39313f
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2025-08-06 14:03:54 +0200

    chore(vitest): Use TypeScript for package script and add documentation

--- packages/vitest-file-snapshots/scripts/fix-declaration-file.ts
+++ packages/vitest-file-snapshots/scripts/fix-declaration-file.ts
@@ -6,5 +6,13 @@ if (!fs.existsSync(generatedDeclarationFile)) {
   throw new Error("Declaration file does not exist.");
 }
 
+/**
+ * Import Vitest at the start of the file
+ *
+ * This ensures that the module augmentation for the custom matchers
+ * happens after the Vitest types are declared
+ */
 const declaration = fs.readFileSync(generatedDeclarationFile);
 fs.writeFileSync(generatedDeclarationFile, `import "vitest";\n${declaration}`);
+
+console.log("Fixed module augmentation in declaration file.");
