c85eec4fb61d19052f0bd3b8d358a1b22b0eb41c
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2025-08-13 08:52:05 +0200

    refactor(vitest): Extract regexp into descriptive constant

--- packages/vitest-file-snapshots/src/matchers/utils.ts
+++ packages/vitest-file-snapshots/src/matchers/utils.ts
@@ -2,6 +2,8 @@ import path from "node:path";
 
 import { TEST_PATH_SEPARATOR } from "./config";
 
+const TEST_EXTENSION_REGEXP = /\.(test|spec)\.[cm]?[tj]sx?$/;
+
 export function parseTestName(currentTestName: string): Array<string> {
   return currentTestName.split(TEST_PATH_SEPARATOR);
 }
@@ -9,5 +11,5 @@ export function parseTestName(currentTestName: string): Array<string> {
 export function parseTestPath(testPath: string, testDir: string): string {
   const relativeTestPath = path.relative(testDir, testPath);
 
-  return relativeTestPath.replace(/\.(test|spec)\.[cm]?[tj]sx?$/, "");
+  return relativeTestPath.replace(TEST_EXTENSION_REGEXP, "");
 }
