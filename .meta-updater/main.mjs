5732b39db0e113689c512b7737e9a410d7479fe7
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2025-11-26 09:37:31 +0100

    ci: Enforce pnpm as package manager

---
+++ .meta-updater/main.mjs
@@ -0,0 +1,29 @@
+import { createUpdateOptions } from "@pnpm/meta-updater";
+
+export default function () {
+  return createUpdateOptions({
+    files: {
+      "package.json": (manifest) => {
+        return {
+          ...manifest,
+          packageManager: "pnpm@10.23.0",
+          engines: {
+            node: "^20 || ^22 || >=24",
+          },
+          devEngines: {
+            runtime: {
+              name: "node",
+              version: ">=24.11 <25",
+              onFail: "error",
+            },
+            packageManager: {
+              name: "pnpm",
+              version: ">=10.21 <11",
+              onFail: "error",
+            },
+          },
+        };
+      },
+    },
+  });
+}
