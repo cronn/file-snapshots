6ddbc703f09727dd237dd9e3ae94d14d0742cea9
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2026-02-19 13:50:20 +0100

    chore(element-snapshot): Export ImageSnapshot and SeparatorSnapshot types

--- packages/element-snapshot/src/index.ts
+++ packages/element-snapshot/src/index.ts
@@ -12,10 +12,12 @@ export type { ContainerSnapshot } from "./browser/container";
 export type { DialogSnapshot } from "./browser/dialog";
 export type { GroupSnapshot, RadiogroupSnapshot } from "./browser/group";
 export type { HeadingSnapshot } from "./browser/heading";
+export type { ImageSnapshot } from "./browser/image";
 export type { InputSnapshot } from "./browser/input";
 export type { LinkSnapshot } from "./browser/link";
 export type { MenuitemSnapshot } from "./browser/list";
 export type { ProgressbarSnapshot } from "./browser/progressbar";
+export type { SeparatorSnapshot } from "./browser/separator";
 export type { TabSnapshot } from "./browser/tab";
 export type { ColumnheaderSnapshot } from "./browser/table";
 
