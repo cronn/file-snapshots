5690e659a5f68811a186495527986e40634b148e
Sebastian Fuchs <sebastian.fuchs@cronn.de>
2026-02-24 15:28:58 +0100

    fix(ci): tsdown logger is not correctly passed to multi config

--- packages/element-snapshot/tsdown.config.ts
+++ packages/element-snapshot/tsdown.config.ts
@@ -4,12 +4,19 @@ import { tsdownConfig } from "@cronn/shared-configs/tsdown";
 
 export default defineConfig((options) => [
   tsdownConfig({
-    entry: ["src/browser-lib.ts"],
-    format: ["iife"],
-    ...options,
-  }),
-  tsdownConfig({
     entry: ["src/index.ts"],
+    format: {
+      esm: {
+        entry: {
+          index: "src/index.ts",
+        },
+      },
+      iife: {
+        entry: {
+          "browser-lib": "src/browser-lib.ts",
+        },
+      },
+    },
     ...options,
   }),
 ]);
